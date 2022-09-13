dd56dce85b45b683768694767f8cfeba77bca510
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-09-13 14:44:25 +0300

    Search input created

--- src/Main/Search/Search.js
+++ src/Main/Search/Search.js
@@ -1,10 +1,21 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, TextInput } from "react-native";
 import React from "react";
-
+import styles from "./Search.style";
+import { AntDesign } from "@expo/vector-icons";
 const Search = () => {
   return (
-    <SafeAreaView>
-      <Text>Search</Text>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>SEARCH</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <AntDesign name="search1" size={24} color="black" />
+        <TextInput
+          style={styles.searchInput}
+          placeholderTextColor="#000"
+          placeholder="What do you want to listen ? "
+        />
+      </View>
     </SafeAreaView>
   );
 };
---
+++ src/Main/Search/Search.style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: "#2e3034",
+    flex: 1,
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 24,
+  },
+  searchContainer: {
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    borderRadius: 5,
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  searchInput: {
+    padding: 15,
+  },
+});
