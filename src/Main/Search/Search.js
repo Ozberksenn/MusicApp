2acaa68fd4f5a026d8418e4bb1d8274027c6f02c
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-09-15 20:00:37 +0300

    Search page edited

--- src/Main/Search/Search.js
+++ src/Main/Search/Search.js
@@ -4,13 +4,19 @@ import styles from "./Search.style";
 import axios from "axios";
 import { AntDesign } from "@expo/vector-icons";
 import GenresCard from "../../components/GenresCard/GenresCard";
+import MusicCard from "../../components/MusicCard/MusicCard";
 const Search = () => {
   const [genres, setGenres] = useState();
   const [focused, setFocused] = useState();
-
+  const [trackData, setTrackData] = useState();
+  const [text, setText] = useState(0);
   useEffect(() => {
     genresData();
   }, []);
+
+  useEffect(() => {
+    tracks();
+  }, [text]);
 
   const genresData = async () => {
     const response = await axios.get(
@@ -19,6 +25,12 @@ const Search = () => {
     setGenres(response.data.genres);
   };
 
+  const tracks = async () => {
+    const response = await axios.get(
+      `https://api.napster.com/v2.2/search/verbose?apikey=YTkxZTRhNzAtODdlNy00ZjMzLTg0MWItOTc0NmZmNjU4Yzk4&per_type_limit=3&query=${text}&type=track`
+    );
+    setTrackData(response.data.search.data.tracks);
+  };
   return (
     <SafeAreaView style={styles.container}>
       <View>
@@ -28,16 +40,19 @@ const Search = () => {
         <AntDesign style={{ left: 5 }} name="search1" size={24} color="black" />
         <TextInput
           onFocus={(value) => setFocused(value)}
+          onChangeText={(value) => setText(value)}
           style={styles.searchInput}
           placeholderTextColor="#000"
           placeholder="What do you want to listen ? "
         />
       </View>
-      {focused ? (
-        <Text>Hello World</Text>
+      {text.length > 0 ? (
+        <FlatList
+          data={trackData}
+          renderItem={({ item }) => <MusicCard data={item} />}
+        />
       ) : (
         <FlatList
-          numColumns={2}
           data={genres}
           renderItem={({ item }) => <GenresCard data={item} />}
         />
