b3134b622b4bea6b734a32cf0adb5896b4db1199
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-09-15 03:40:08 +0300

    if there is a user, enter the system

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import store from "./src/redux/Store";
 import TabBar from "./src/components/TabBar/TabBar";
 
@@ -44,14 +45,33 @@ const Login = () => {
 };
 
 const Content = () => {
+  const [localData, setLocalData] = useState(null);
+  const { userInfo } = useSelector((state) => state.user);
+  const getLocal = async () => {
+    const response = await AsyncStorage.getItem("userKey");
+    const local = response ? JSON.parse(response) : null;
+    setLocalData(local);
+    console.log("control", local);
+  };
+  useEffect(() => {
+    getLocal();
+  }, []);
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Main" component={Main} />
-      <Stack.Screen name="ProfileDetail" component={ProfileDetail} />
-      <Stack.Screen name="Theme" component={Theme} />
-      <Stack.Screen name="EditProfile" component={EditProfile} />
-      <Stack.Screen name="FeaturedDetail" component={FeaturedDetail} />
+      {!localData ? (
+        <>
+          <Stack.Screen name="Login" component={Login} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Main" component={Main} />
+          <Stack.Screen name="ProfileDetail" component={ProfileDetail} />
+          <Stack.Screen name="Theme" component={Theme} />
+          <Stack.Screen name="EditProfile" component={EditProfile} />
+          <Stack.Screen name="FeaturedDetail" component={FeaturedDetail} />
+        </>
+      )}
     </Stack.Navigator>
   );
 };
